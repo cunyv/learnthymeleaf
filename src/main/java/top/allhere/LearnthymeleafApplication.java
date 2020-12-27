f07a42e7cbbb2c992ca6a22480589e34b10a8446
cunyv <2468697909@qq.com>
2020-12-27 18:16:01 +0800

    整合mybatis

--- src/main/java/top/allhere/LearnthymeleafApplication.java
+++ src/main/java/top/allhere/LearnthymeleafApplication.java
@@ -1,5 +1,6 @@
 package top.allhere;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@MapperScan("top.allhere.repository")
 @Controller
 public class LearnthymeleafApplication {
 
