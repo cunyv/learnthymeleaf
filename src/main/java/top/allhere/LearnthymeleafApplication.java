c842fc02100b0c614ab7ee81b4205d46dba4ee17
zhangjian <2468697909@qq.com>
2020-12-27 16:28:11 +0800

    测试项目运行正常

--- src/main/java/top/allhere/LearnthymeleafApplication.java
+++ src/main/java/top/allhere/LearnthymeleafApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
+@RestController
 public class LearnthymeleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnthymeleafApplication.class, args);
 	}
 
+	@GetMapping("/name")
+	public String getName(){
+		return "zhangjian";
+	}
 }
