74660d3f50b921927638121392cd524bbabbd171
cunyv <2468697909@qq.com>
2021-01-19 13:31:59 +0800

    使用mybatis-plus代码生成器生成代码

--- src/main/java/top/allhere/controller/UserController.java
+++ src/main/java/top/allhere/controller/UserController.java
@@ -3,6 +3,7 @@ package top.allhere.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +27,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
-    public User userMsg(){
-        return userService.getById(1L);
+    @GetMapping("/users/{id}")
+    public User userMsg(@PathVariable("id") Long id){
+        return userService.getById(id);
     }
 }
 
